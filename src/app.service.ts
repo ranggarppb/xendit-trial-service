f9f9a25276ff688a84bbbf60c8d8ba55c8bebb56
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2022-01-21 00:15:52 +0700

    feat: add invalidation flow

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,21 +6,29 @@ import fetch from 'node-fetch';
 export class AppService {
   constructor(private readonly redisCacheService: RedisCacheService) {}
 
-  async getHello(): Promise<any> {
-    const config = await this.redisCacheService.get('config');
-
-    if (!config) {
-      console.log('Getting data directly from config server');
-      const response = await fetch('http://localhost:8080/service/config');
-      const config = await response.text();
-      console.log(config);
-      console.log('Set config in cache');
-      await this.redisCacheService.set('config', config, { ttl: 31556952 });
-      return config;
-    }
-
-    console.log('Getting data from cache');
+  private async getConfigAndStoreInCache() {
+    console.log('Getting data directly from config server');
+    const response = await fetch('http://localhost:8080/service/config');
+    const config = await response.text();
     console.log(config);
+    console.log('Set config in cache');
+    await this.redisCacheService.set('config', config, { ttl: 31556952 });
     return config;
+  }
+
+  async getHello(useCache = true): Promise<any> {
+    if (useCache) {
+      const config = await this.redisCacheService.get('config');
+
+      if (!config) {
+        return this.getConfigAndStoreInCache();
+      }
+
+      console.log('Getting data from cache');
+      console.log(config);
+      return config;
+    } else {
+      return this.getConfigAndStoreInCache();
+    }
   }
 }
