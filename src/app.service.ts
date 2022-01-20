518c4c57b58a9721f1252730e73a46e42bb70a68
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2022-01-20 18:35:15 +0700

    feat: enable calling config service api

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import fetch from 'node-fetch';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<any> {
+    const response = await fetch('http://localhost:8080/service/config');
+    const body = await response.text();
+    return body;
   }
 }
