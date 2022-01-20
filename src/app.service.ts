0f101a4447f910e601dcf07fe714bb3418d5c427
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2022-01-20 23:37:06 +0700

    refactor: redis module

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,24 +1,26 @@
-import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
-import { Cache } from 'cache-manager';
+import { Injectable } from '@nestjs/common';
+import { RedisCacheService } from './redis/redis.service';
 import fetch from 'node-fetch';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+  constructor(private readonly redisCacheService: RedisCacheService) {}
 
   async getHello(): Promise<any> {
-    const config = await this.cacheManager.get('config');
+    const config = await this.redisCacheService.get('config');
 
     if (!config) {
       console.log('Getting data directly from config server');
       const response = await fetch('http://localhost:8080/service/config');
       const config = await response.text();
+      console.log(config);
       console.log('Set config in cache');
-      await this.cacheManager.set('config', config, { ttl: 31556952 });
+      await this.redisCacheService.set('config', config, { ttl: 31556952 });
       return config;
     }
 
     console.log('Getting data from cache');
+    console.log(config);
     return config;
   }
 }
