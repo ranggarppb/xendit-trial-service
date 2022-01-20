5fd58a70a468c1596128fd563edfdc981b96bd26
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2022-01-20 21:13:01 +0700

    feat: add redis cache for config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KafkaModule } from './kafka/kafka.module';
 import { ConfigurationServiceConsumer } from './config.consumer';
 
 @Module({
-  imports: [KafkaModule],
+  imports: [
+    KafkaModule,
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ConfigurationServiceConsumer],
 })
