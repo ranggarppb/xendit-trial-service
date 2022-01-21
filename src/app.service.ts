ea317854edd2ef579232e98821115020c0055c46
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2022-01-21 09:42:34 +0700

    feat: add default config if config service is down

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,25 +8,28 @@ export class AppService {
 
   private async getConfigAndStoreInCache() {
     console.log('Getting data directly from config server');
-    const response = await fetch('http://localhost:8080/service/config');
-    const config = await response.text();
-    console.log('Set config in cache');
-    await this.redisCacheService.set('config', config, { ttl: 31556952 });
-    return config;
+    try {
+      const response = await fetch('http://localhost:8080/service/config');
+      const config = await response.json();
+      console.log('Set config in cache');
+      await this.redisCacheService.set('configuration', config, {
+        ttl: 31556952,
+      });
+      return config;
+    } catch (error) {
+      console.log('Error getting data from config server');
+      return 'Default config';
+    }
   }
 
-  async getHello(useCache = true): Promise<any> {
-    if (useCache) {
-      const config = await this.redisCacheService.get('configuration');
+  async getRequest(): Promise<any> {
+    const config = await this.redisCacheService.get('configuration');
 
-      if (!config) {
-        return this.getConfigAndStoreInCache();
-      }
-
-      console.log('Getting data from cache');
-      return config;
-    } else {
+    if (!config) {
       return this.getConfigAndStoreInCache();
     }
+
+    console.log('Getting data from cache');
+    return config;
   }
 }
